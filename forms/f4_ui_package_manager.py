48da0d13247dd1284406c7628604d81ab2008f58
akey <darkmark404@protonmail.com>
2026-03-09 19:45:39 -0600

    feat (f4): delete '*' from default Search LineEdit

--- forms/f4_ui_package_manager.py
+++ forms/f4_ui_package_manager.py
@@ -298,7 +298,7 @@ class Ui_Widget(object):
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"RQT2 IDE / Gestor de paquetes", None))
         self.LABELSearch.setText(QCoreApplication.translate("Widget", u"Buscar", None))
-        self.EDITSearch.setText(QCoreApplication.translate("Widget", u"*", None))
+        #self.EDITSearch.setText(QCoreApplication.translate("Widget", u"*", None))
         self.CBOPT1Search.setText(QCoreApplication.translate("Widget", u"Herramientas", None))
         self.CBOPT2Search.setText(QCoreApplication.translate("Widget", u"Dependencias", None))
         #self.BUTTONInstall0.setText(QCoreApplication.translate("Widget", u"Instalar", None))
