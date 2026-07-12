fd8667f817ed37c7ba567411eb269a5ddbebdfff
adnksharp <darkmark404@protonmail.com>
2026-07-11 20:43:58 -0600

    feat: config new rqt window

--- forms/test-widgets.py
+++ forms/test-widgets.py
@@ -22,6 +22,7 @@ from g2_ui_compiler import Ui_Widget as Ui_Compiler
 from g3_ui_twist_controller import Ui_Widget as Ui_Twist
 from g4_ui_ssh import Ui_Widget as Ui_Ssh
 from g5_ui_rviz2 import Ui_Widget as Ui_Rviz
+from g7_ui_rqt import Ui_Widget as Ui_Rqt
 
 try:
     from ..utils.base_window import DemoWindow
@@ -106,11 +107,12 @@ if __name__ == "__main__":
         #(Ui_WizConfig, "RQT2 IDE / Versión de ROS2", False, False),
         #(Ui_WizProgress, "RQT2 IDE / Progreso de Instalación", False, False),
         #(Ui_WizClose, "RQT2 IDE / Finalizar Instalación", False, False),
-        (Ui_TextEditor, "RQT2 IDE / *", True, True),
+        #(Ui_TextEditor, "RQT2 IDE / *", True, True),
         #(Ui_Compiler, "RQT2 IDE / *", True, False),
         #(Ui_Twist, "RQT2 IDE / *", True, False),
         #(Ui_Ssh, "RQT2 IDE / *", True, False),
-        (Ui_Rviz, "RQT2 IDE / *", True, False)
+        #(Ui_Rviz, "RQT2 IDE / *", True, False),
+        (Ui_Rqt, "RQT2 IDE / *", True, False),
     ]
 
     for ui_class, title, show_daemon, show_tab in mapping:
